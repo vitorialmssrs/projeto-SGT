bfa5f0cae9823d8ec1038ba63027ccc08099d027
otavio <otaviomiguel1301@gmail.com>
2024-02-29 16:31:26 -0300

    Tela Hospede com midlayout
    feito a tela hospede com midlayout (iniciando)

--- SGTProject/src/main/java/modelo/Hospede.java
+++ SGTProject/src/main/java/modelo/Hospede.java
@@ -5,8 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Hospede extends JFrame {
+
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -35,8 +43,16 @@ public class Hospede extends JFrame {
 		setBounds(100, 100, 1920, 1080);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		setExtendedState(MAXIMIZED_BOTH);
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[][][][1894px]", "[][][1032px]"));
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton, "cell 1 0,aligny top");
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Châteu_Imperial-removebg-preview 4.png"));
+		contentPane.add(lblNewLabel, "cell 3 2,aligny top");
 	}
 
 }
