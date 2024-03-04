9d35c6711a25b37dec64b12e69f576d3547bb96f
otavio <otaviomiguel1301@gmail.com>
2024-03-04 17:04:23 -0300

    Funcionalidade Funcionarios Com MigLayout
    foi alterado a tela para que possa se ajustar em diversos tipos de monitores

--- SGTProject/src/main/java/visao/FuncionalidadeFuncionario.java
+++ SGTProject/src/main/java/visao/FuncionalidadeFuncionario.java
@@ -5,17 +5,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-
 import java.awt.GridLayout;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import net.miginfocom.swing.MigLayout;
 import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
-import javax.swing.JTextField;
+import javax.swing.JButton;
 import javax.swing.SwingConstants;
 
 public class FuncionalidadeFuncionario extends JFrame {
@@ -50,65 +46,57 @@ public class FuncionalidadeFuncionario extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		JLabel lblImagemHotel = new JLabel("");
-		lblImagemHotel.setBounds(0, 0, 952, 1042);
-		lblImagemHotel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Hotel 2.png"));
-		contentPane.add(lblImagemHotel);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(227, 236, 226));
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JButton btnCheckin = new JButton("Solicitar Limpeza");
-		btnCheckin.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons limpeza.png"));
-		btnCheckin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnCheckin.setForeground(new Color(255, 255, 255));
-		btnCheckin.setFont(new Font("Tahoma", Font.BOLD, 60));
-		btnCheckin.setBackground(new Color(81, 108, 81));
-		btnCheckin.setBounds(944, 284, 923, 147);
-		contentPane.add(btnCheckin);
+		JLabel lblFTHotel = new JLabel("");
+		lblFTHotel.setBounds(0, 0, 911, 1080);
+		lblFTHotel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Hotel 2.png"));
+		panel.add(lblFTHotel);
 		
-		JButton btnCheckout = new JButton("Solicitar Conserto");
-		btnCheckout.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons conserto.png"));
-		btnCheckout.setForeground(new Color(255, 255, 255));
-		btnCheckout.setFont(new Font("Tahoma", Font.BOLD, 60));
-		btnCheckout.setBackground(new Color(66, 142, 66));
-		btnCheckout.setBounds(944, 512, 923, 147);
-		contentPane.add(btnCheckout);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(227, 236, 226));
+		contentPane.add(panel_1);
+		panel_1.setLayout(new MigLayout("", "[150][300][150]", "[200][][100][50][200][50][200][50][200][50][100]"));
 		
-		JButton btnReservarEspaco = new JButton("Atualizar Espaço");
-		btnReservarEspaco.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons atualizacao espaco.png"));
-		btnReservarEspaco.setForeground(new Color(255, 255, 255));
-		btnReservarEspaco.setFont(new Font("Tahoma", Font.BOLD, 60));
-		btnReservarEspaco.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnReservarEspaco.setBackground(new Color(109, 164, 109));
-		btnReservarEspaco.setBounds(944, 740, 923, 147);
-		contentPane.add(btnReservarEspaco);
+		JLabel lblNewLabel = new JLabel("Caro Funcionario");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 40));
+		panel_1.add(lblNewLabel, "cell 1 0,alignx center");
 		
-		JLabel lblInstrucaoCliente = new JLabel("Caro Funcionario");
-		lblInstrucaoCliente.setHorizontalAlignment(SwingConstants.CENTER);
-		lblInstrucaoCliente.setForeground(new Color(1, 50, 1));
-		lblInstrucaoCliente.setFont(new Font("Tahoma", Font.BOLD, 40));
-		lblInstrucaoCliente.setBounds(1244, 35, 378, 65);
-		contentPane.add(lblInstrucaoCliente);
+		JLabel lblNewLabel_1 = new JLabel("Escolha uma opção de operação");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 30));
+		panel_1.add(lblNewLabel_1, "cell 1 2,alignx center");
 		
-		JButton btnSair = new JButton("Sair");
-		btnSair.setBackground(new Color(227, 236, 226));
-		btnSair.setForeground(new Color(1, 50, 1));
-		btnSair.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnSair.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Vector sair.png"));
-		btnSair.setBounds(1716, 968, 151, 39);
-		btnSair.setBorder(new LineBorder(new Color(36, 169, 36)));
-		contentPane.add(btnSair);
+		JButton btnNewButton = new JButton("Solicitar Limpeza");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 60));
+		btnNewButton.setBackground(new Color(81, 108, 81));
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons limpeza.png"));
+		panel_1.add(btnNewButton, "cell 1 4,grow");
 		
-		JLabel lblNewLabel = new JLabel("Escolha uma opção de operação");
-		lblNewLabel.setForeground(new Color(1, 50, 1));
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblNewLabel.setBounds(1194, 125, 494, 80);
-		contentPane.add(lblNewLabel);
+		JButton btnNewButton_1 = new JButton("Solicitar Conserto");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 60));
+		btnNewButton_1.setForeground(new Color(255, 255, 255));
+		btnNewButton_1.setBackground(new Color(66, 142, 66));
+		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons conserto.png"));
+		panel_1.add(btnNewButton_1, "cell 1 6,grow");
+		
+		JButton btnNewButton_2 = new JButton("Atualizar Espaço");
+		btnNewButton_2.setFont(new Font("Tahoma", Font.BOLD, 60));
+		btnNewButton_2.setBackground(new Color(109, 164, 109));
+		btnNewButton_2.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Icons atualizacao espaco.png"));
+		btnNewButton_2.setForeground(new Color(255, 255, 255));
+		panel_1.add(btnNewButton_2, "cell 1 8,grow");
+		
+		JButton btnNewButton_3_1 = new JButton("Sair");
+		btnNewButton_3_1.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\projeto-SGT\\SGTProject\\src\\main\\resources\\img\\Vector sair.png"));
+		btnNewButton_3_1.setBackground(new Color(227, 236, 226));
+		btnNewButton_3_1.setForeground(new Color(1, 50, 1));
+		btnNewButton_3_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panel_1.add(btnNewButton_3_1, "cell 2 10");
 	}
 }
