619d5c2a93780e2c0db349cee944679263645337
Vitória <vitoria.ls10@aluno.ifsc.edu.br>
2024-03-08 20:54:08 -0300

    Criação do código de IEspacoDAO

--- SGTProject/src/main/java/controle/IEspacosDAO.java
+++ SGTProject/src/main/java/controle/IEspacosDAO.java
@@ -1,6 +1,20 @@
 package controle;
 
+import java.util.ArrayList;
+
+import modelo.EspacoHotel;
+
 public interface IEspacosDAO {
+	
+	public int inserirEspacos (EspacoHotel end);
+	
+	public ArrayList<EspacoHotel> listarEnderecos();
+
+	public boolean atualizarEndereco(EspacoHotel end);
+
+	public boolean removerEndereco(EspacoHotel end);
+
+	public EspacoHotel buscarEnderecoPorCep(int cep);
 	
 
 
