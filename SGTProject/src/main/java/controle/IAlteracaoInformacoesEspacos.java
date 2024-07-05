a058287f8390417ebf85887191ba10a3277fe4c0
Vitória Lemos Soares <vitoria.ls10@aluno.ifsc.edu.br>
2024-07-05 10:18:01 -0300

    classes da nova tela criada

---
+++ SGTProject/src/main/java/controle/IAlteracaoInformacoesEspacos.java
@@ -0,0 +1,12 @@
+package controle;
+
+import java.util.ArrayList;
+
+public class IAlteracaoInformacoesEspacos {
+
+	public int inserirAlteracao (AlteracaoInformcoesEspacos end);
+	public boolean atualizarAlteracao(AlteracaoInformcoesEspacos end);
+	public boolean removerAlteracao(AlteracaoInformcoesEspacos end);
+	
+
+}
