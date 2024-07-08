db276140608869e5fb4bf9f13be18a9131e71b90
vi <vitoria.j13@aluno.ifsc.edu.br>
2024-07-08 20:05:44 -0300

    Correção TelaAlteraçãoInformaçõesEspaços

--- SGTProject/src/main/java/visao/TelaAlteracaoInformacoesEspacos.java
+++ SGTProject/src/main/java/visao/TelaAlteracaoInformacoesEspacos.java
@@ -5,17 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
 import controle.EspacosDAO;
-import controle.ManutencaoEspacosDAO;
 import modelo.EspacoHotel;
-import modelo.Hospede;
-import modelo.ManutencaoEspacos;
 
 import java.awt.Color;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 import java.awt.Font;
 import javax.swing.JButton;
@@ -27,7 +22,6 @@ import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 import javax.swing.ImageIcon;
-import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
@@ -73,7 +67,7 @@ public class TelaAlteracaoInformacoesEspacos extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblTitulo = new JLabel("Alteração de informações das dependencias do hotel");
-		lblTitulo.setBounds(466, 93, 1064, 49);
+		lblTitulo.setBounds(349, 133, 1064, 49);
 		lblTitulo.setForeground(new Color(1, 50, 1));
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 40));
 		contentPane.add(lblTitulo);
@@ -99,32 +93,32 @@ public class TelaAlteracaoInformacoesEspacos extends JFrame {
 		JLabel lblNewLabel = new JLabel("Selecione o espaço.");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblNewLabel.setForeground(new Color(1, 50, 1));
-		lblNewLabel.setBounds(604, 324, 244, 27);
+		lblNewLabel.setBounds(447, 267, 244, 27);
 		contentPane.add(lblNewLabel);
 		
 		textDiaA = new JTextField();
-		textDiaA.setBounds(615, 486, 159, 32);
+		textDiaA.setBounds(448, 429, 159, 32);
 		contentPane.add(textDiaA);
 		textDiaA.setColumns(10);
 		
 		textDiaF = new JTextField();
-		textDiaF.setBounds(614, 584, 159, 32);
+		textDiaF.setBounds(447, 527, 159, 32);
 		contentPane.add(textDiaF);
 		textDiaF.setColumns(10);
 		
 		textHoraA = new JTextField();
-		textHoraA.setBounds(1106, 482, 159, 32);
+		textHoraA.setBounds(949, 425, 159, 32);
 		contentPane.add(textHoraA);
 		textHoraA.setColumns(10);
 		
 		textHoraF = new JTextField();
-		textHoraF.setBounds(1106, 584, 159, 32);
+		textHoraF.setBounds(949, 527, 159, 32);
 		contentPane.add(textHoraF);
 		textHoraF.setColumns(10);
 		
 		textCapacidade = new JTextField();
 		textCapacidade.setBackground(new Color(255, 255, 255));
-		textCapacidade.setBounds(1106, 378, 159, 32);
+		textCapacidade.setBounds(949, 327, 159, 32);
 		contentPane.add(textCapacidade);
 		textCapacidade.setColumns(10);
 		
@@ -157,9 +151,9 @@ public class TelaAlteracaoInformacoesEspacos extends JFrame {
 				String saida = horasaida.format(formattertime);
 				textHoraF.setText(saida);
 				
-				//Capacidade
-				 int capacidade = espaco.getCapacidade();
-                 textCapacidade.setText(String.valueOf(capacidade));
+				// Capacidade
+                int capacidade = espaco.getCapacidade();
+                textCapacidade.setText(String.valueOf(capacidade));
 				
 			}
 		});
@@ -170,25 +164,25 @@ public class TelaAlteracaoInformacoesEspacos extends JFrame {
 			comboBoxEspaco.addItem(espacoHotel);
 		}
 		
-		comboBoxEspaco.setBounds(604, 370, 263, 49);
+		comboBoxEspaco.setBounds(447, 313, 263, 49);
 		contentPane.add(comboBoxEspaco);
 	
 		JLabel lblNewLabel_2 = new JLabel("Dia da abertura ");
 		lblNewLabel_2.setForeground(new Color(1, 50, 1));
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_2.setBounds(615, 448, 233, 27);
+		lblNewLabel_2.setBounds(448, 391, 233, 27);
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_4 = new JLabel("Horario de abertura");
 		lblNewLabel_4.setForeground(new Color(1, 50, 1));
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_4.setBounds(1097, 444, 233, 27);
+		lblNewLabel_4.setBounds(940, 387, 233, 27);
 		contentPane.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Horario de fechamento");
 		lblNewLabel_5.setForeground(new Color(1, 50, 1));
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_5.setBounds(1097, 546, 244, 27);
+		lblNewLabel_5.setBounds(940, 489, 244, 27);
 		contentPane.add(lblNewLabel_5);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
@@ -216,20 +210,43 @@ public class TelaAlteracaoInformacoesEspacos extends JFrame {
 		
 		JLabel lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setIcon(new ImageIcon(TelaManutencaoEspacos.class.getResource("/imagens/Châteu_Imperial-removebg-preview 4.png")));
-		lblNewLabel_6.setBounds(246, 43, 226, 174);
+		lblNewLabel_6.setBounds(113, 79, 226, 174);
 		contentPane.add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_1 = new JLabel("Dia em que o espaço está fechado");
 		lblNewLabel_1.setForeground(new Color(1, 50, 1));
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(614, 546, 367, 27);
+		lblNewLabel_1.setBounds(447, 489, 367, 27);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_9 = new JLabel("Capacidade de pessoas no espaço");
 		lblNewLabel_9.setForeground(new Color(1, 50, 1));
 		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_9.setBounds(1097, 334, 376, 33);
+		lblNewLabel_9.setBounds(940, 283, 376, 33);
 		contentPane.add(lblNewLabel_9);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnSalvar.setForeground(new Color(252, 251, 244));
+		btnSalvar.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnSalvar.setBackground(new Color(66, 142, 66));
+		btnSalvar.setBounds(447, 660, 176, 39);
+		contentPane.add(btnSalvar);
+		
+		JButton btnSair_1 = new JButton("<- | Sair");
+		btnSair_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		btnSair_1.setForeground(new Color(252, 251, 244));
+		btnSair_1.setFont(new Font("Tahoma", Font.BOLD, 17));
+		btnSair_1.setBackground(new Color(1, 50, 1));
+		btnSair_1.setBounds(949, 660, 176, 39);
+		contentPane.add(btnSair_1);
 		
 		
 	}
